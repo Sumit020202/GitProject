31020430a4ca8de422c8364b84f06ffba209973d
Sumit Singh <155011889+Sumit020202@users.noreply.github.com>
2024-12-18 19:41:48 +0000

    new

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -5,21 +5,37 @@ import './App.css'
 
 function App() {
   
-let counter = 10
+  let [counter, setCounter] = useState(10)
+
+//let counter = 10
 
 const addValue = () => {
-console.log("value added",counter);
-counter = counter + 1
+//console.log("value added",counter);
+//counter = counter + 1
+setCounter(counter + 2)
 }
+
+const removeValue = () => {
+  if(counter > 0){
+    setCounter(counter - 1)
+  }else{
+    console.log("Counter is already at 0, Can't decrease further.")
+  }
+}
+
+
   return (
     <>
      <h1>Custom App</h1>
      <h2>Counter Value : {counter}</h2>
 
      <button
-     onClick={addValue}>Add Value</button>
+     onClick={addValue}
+     >Add Value {counter}</button>
      <br/>
-     <button>Remove Value</button>
+     <button
+     onClick={removeValue}>Remove Value {counter}</button>
+     <p>footer: {counter}</p>
     </>
   )
 }
